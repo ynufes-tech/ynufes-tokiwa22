fb2299cd10ff1b3f55344415e88abf05b596c568
Shion Ichikawa <shion1305@gmail.com>
2025-11-03 07:43:32 +0900

    🐛 (SecondRowSwiper) 画像の大きさのcssを修正

--- src/components/home/SecondRowSwiper.vue
+++ src/components/home/SecondRowSwiper.vue
@@ -73,6 +73,11 @@ import { publicPath } from "@/main";
         justify-content: center;
         align-items: center;
         background: radial-gradient(#1e3272, #15084f);
+        > img {
+          width: 100%;
+          height: 100%;
+          object-fit: contain;
+        }
       }
     }
 
